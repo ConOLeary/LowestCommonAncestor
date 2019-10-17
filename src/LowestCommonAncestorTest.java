066d7ac63d2d4d2eed4584af7a716e89e1e8a3fc
Con <conolaoghaire@gmail.com>
2019-10-17 21:20:22 +0100

    Initial tests satisfied
    Initial tests all set up and being satisfied. Tidied up code.
    Technically I have written my implementation before my tests, but I have always known the tests I have wanted on paper, so I don't feel as though I have adjusted my tests to fit my implementation whatsoever.

--- src/LowestCommonAncestorTest.java
+++ src/LowestCommonAncestorTest.java
@@ -3,36 +3,55 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LowestCommonAncestorTest {
-	
-	public static void main(String args[])
+	LowestCommonAncestor lcaImplementation;
+	Node<Integer> sampleHead;
+	BinaryTree sampleTree;
+	Node<Integer> node1;
+	Node<Integer> node2;
+	Node<Integer> node3;
+	Node<Integer> node4;
+	Node<Integer> node5;
+	Node<Integer> node6;
+	Node<Integer> ancestor;
+	public LowestCommonAncestorTest()
 	{
-
-	}
-
-	
-	@Test
-	public void testPopulation() {
-		Node<Integer> sampleHead= new Node<Integer>(69);
-		BinaryTree sampleTree= new BinaryTree(sampleHead);
-		Node<Integer> node1= new Node<Integer>(420);
-		Node<Integer> node2= new Node<Integer>(123);
-		Node<Integer> node3= new Node<Integer>(4210);
-		Node<Integer> node4= new Node<Integer>(12);
-		Node<Integer> node5= new Node<Integer>(9);
-		Node<Integer> node6= new Node<Integer>(1231);
+		lcaImplementation= new LowestCommonAncestor();
+		sampleHead= new Node<Integer>(69);
+		sampleTree= new BinaryTree(sampleHead);
+		node1= new Node<Integer>(420);
+		node2= new Node<Integer>(123);
+		node3= new Node<Integer>(4210);
+		node4= new Node<Integer>(12);
+		node5= new Node<Integer>(9);
+		node6= new Node<Integer>(1231);
 		sampleTree.push(node1);
 		sampleTree.push(node2);
 		sampleTree.push(node3);
 		sampleTree.push(node4);
 		sampleTree.push(node5);
 		sampleTree.push(node6);
-		System.out.print("data @ rightChild of head :"+sampleTree.head.getRightChild().getData().toString());
-		assertEquals("a","a");
-		LowestCommonAncestor lcaImplementation= new LowestCommonAncestor();
-		Node<Integer> ancestor= lcaImplementation.lowestCommonAncestor(sampleHead, node5, node3);
-		assertEquals(ancestor,sampleHead);
-		assertEquals(1,1);
-		System.out.print("buffffffer");
 	}
-
+	
+	@Test
+	public void LCAnormal() {
+		System.out.print("data @ rightChild of head :"+sampleTree.head.getRightChild().getData().toString());
+		ancestor= lcaImplementation.lowestCommonAncestor(sampleHead, node5, node3);
+		assertEquals(ancestor,sampleHead);
+	}
+	@Test
+	public void LCAnullInputs() {
+		ancestor= lcaImplementation.lowestCommonAncestor(null, null, null);
+		assertEquals(ancestor,null);
+		ancestor= lcaImplementation.lowestCommonAncestor(sampleHead, null, null);
+		assertEquals(ancestor,null);
+		ancestor= lcaImplementation.lowestCommonAncestor(sampleHead, node1, null);
+		assertEquals(ancestor,null);
+	}
+	@Test
+	public void LCAotherEdgeCases() {
+		ancestor= lcaImplementation.lowestCommonAncestor(sampleHead, node1, node1);
+		assertEquals(ancestor,null);
+		ancestor= lcaImplementation.lowestCommonAncestor(node1, node1, node1);
+		assertEquals(ancestor,null);
+	}
 }
